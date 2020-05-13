4208d0fa35dbe361be20c5315c15f8c4a86a98c6
Andrew Yates <andrew@laneyates.com>
2020-05-13 12:21:21 -0400

    transparent 'S', move and scale filler image

--- source.js
+++ source.js
@@ -8,11 +8,36 @@ var img = new Image();
 img.onload = function(){
   ctx.drawImage(img, 0, 0);
 }
-img.src = "base_logo.png";
+img.src = "base_logo_with_cutout.png";
+
+var scale = 1;
+var xPos = 200;
+var yPos = 500;
+var scaler = document.getElementById("scale");
+var columner = document.getElementById("xPos");
+var rower = document.getElementById("yPos");
+scaler.oninput = function(){
+  scale = (101-scaler.value)/50;
+  console.log(scale);
+  draw();
+}
+columner.oninput = function(){
+  xPos = columner.value;
+  console.log(xPos);
+  draw();
+}
+rower.oninput = function(){
+  yPos = rower.value;
+  console.log(yPos);
+  draw();
+}
+
 
 var cimg = new Image();
 cimg.onload = function(){
-  ctx.drawImage(cimg, 200, 500, 100, 100);
+  draw();
+  //ctx.drawImage(cimg, 200, 500, cimg.width/scale, cimg.height/scale);
+  //ctx.drawImage(img, 0, 0);
 }
 
 file.onchange = function(event){
@@ -22,6 +47,11 @@ file.onchange = function(event){
 }
 
 
+function draw(){
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.drawImage(cimg, xPos, yPos, cimg.width/scale, cimg.height/scale);
+  ctx.drawImage(img, 0, 0);
+}
 
 
 
